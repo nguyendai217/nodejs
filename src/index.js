bfb3eaacc7ed07466cc847650180cc0802970757
nguyendai217 <nguyendai2171998@gmail.com>
2021-03-28 16:14:48 +0700

    add layout hbs

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ const app = express();
 const path = require('path');
 const handlebars = require('express-handlebars');
 const port = 3000;
-app.engine('handlebars',handlebars());
+
+//config handlebars
+app.engine('hbs',handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.listen(3000, () => {
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(port, () => {
     console.log("App listening on port 3000")
 })
 
